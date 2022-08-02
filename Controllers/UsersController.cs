1cb656c0b18955a848630c5a2f8737217254ae7b
TheRealYaseen <s.syedyaseen.s@gmail.com>
2022-08-02 02:35:47 +0100

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.Data;
+using SocialMediaApp.Entities;
+
+namespace SocialMediaApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    
+    // GET
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> Users()
+    {
+        return await _context.Users.ToListAsync();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    } 
+}
